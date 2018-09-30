3742c15206429d93a2e6f16c2c9997385b943fe3
Jisoo <cha21@purdue.edu>
2018-09-30 06:30:31 -0400

    game over screen
    game over when NROWS >=19.
    Also made gameover screen and function to reset and restart the game.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     //init
     var WIDTH, HEIGHT;
     var x = 150,
@@ -17,10 +17,13 @@ $(function() {
     var firstBallOnGroundIndex = 0;
     var numBallsOnGround = 0;
     var ballsLength = 0;
+    var bricklevel = 2;
+    var gameoff = false;
 
-
-    var BRICKHEIGHT = 0, BRICKWIDTH = 0;
-    var NROWS = 0, NCOLS = 0;
+    var BRICKHEIGHT = 0,
+        BRICKWIDTH = 0;
+    var NROWS = 0,
+        NCOLS = 0;
 
     function init() {
         //canvas 가져오기
@@ -45,31 +48,32 @@ $(function() {
         window.requestAnimationFrame(draw);
         window.requestAnimationFrame(draw);
     }
+
     function draw() {
 
         clear();
         printBalls();
         //ball(x, y, radius);
         $('#score').text("Score: " + score);
-        if(gameOn){
-            for(m = 0; m < balls.length; m++){
+        if (gameOn) {
+            for (m = 0; m < balls.length; m++) {
                 balls[m].x += balls[m].dx;
                 balls[m].y += balls[m].dy;
-                if(balls[m].x > WIDTH - radius || balls[m].x < 0 + radius){
-                  balls[m].dx = -balls[m].dx;
-                  balls[m].bounced = 1;
+                if (balls[m].x > WIDTH - radius || balls[m].x < 0 + radius) {
+                    balls[m].dx = -balls[m].dx;
+                    balls[m].bounced = 1;
                 }
-                if(balls[m].y < 0 + radius){
-                  balls[m].dy = -balls[m].dy;
-                  balls[m].bounced = 1;
+                if (balls[m].y < 0 + radius) {
+                    balls[m].dy = -balls[m].dy;
+                    balls[m].bounced = 1;
                 }
 
 
                 // ball hit ground
-                if( balls[m].y > HEIGHT - radius-2 && balls[m].bounced == 1){
+                if (balls[m].y > HEIGHT - radius - 2 && balls[m].bounced == 1) {
                     //alert(numBallsOnGround);
                     numBallsOnGround++;
-                    if(ballHitGround == 0){
+                    if (ballHitGround == 0) {
                         ballHitGround = 1;
 
                         firstBallOnGroundIndex = m;
@@ -83,11 +87,13 @@ $(function() {
                     //   balls[m].dy = 0;
                     //   balls[m].dx = 0;
                     // }
-                    if(numBallsOnGround == ballsLength){
-                        for(j = 0; j < balls.length; ++j){ // brings all balls together after all the balls are on ground
+
+                    if (numBallsOnGround == ballsLength && NROWS < 19) {
+                        for (j = 0; j < balls.length; ++j) { // brings all balls together after all the balls are on ground
                             balls[j].x = balls[firstBallOnGroundIndex].x;
                             balls[j].y = balls[firstBallOnGroundIndex].y;
                         }
+                        bricklevel++;
                         gameOn = false;
                         NROWS++;
                         brickrow = [];
@@ -96,23 +102,26 @@ $(function() {
                             brick = {};
                             brick.color = getRndColor();
                             brick.appear = Math.round(Math.random() + 0.1);
-                            if(brick.appear == 0){
-                              emptyPositions.push(j);
+                            if (brick.appear == 0) {
+                                emptyPositions.push(j);
                             }
-                            brick.number = 2;
-                            brickrow[j] = brick;//Math.round(Math.random() + 0.2);
+                            brick.number = bricklevel;
+                            brickrow[j] = brick; //Math.round(Math.random() + 0.2);
                         }
-                        randomEmptyPos = Math.floor(Math.random()*(emptyPositions.length));// to put an extra new ball in a random empty position
+                        randomEmptyPos = Math.floor(Math.random() * (emptyPositions.length)); // to put an extra new ball in a random empty position
                         newBallPos = emptyPositions[randomEmptyPos];
                         brickrow[newBallPos].appear = 2;
                         bricks.unshift(brickrow);
+
+                    } else if (NROWS >= 19) {
+                        gameOn = true;
+                        gameoff = true;
+                        gameover();
+                        return;
                     }
-
-                    //if()
                 }
-
             }
-          anim = window.requestAnimationFrame(draw);
+            anim = window.requestAnimationFrame(draw);
         }
         // else{
         //
@@ -122,37 +131,35 @@ $(function() {
         //draw bricks
         for (i = 0; i < NROWS; i++) {
             for (j = 0; j < NCOLS; j++) {
-                if(bricks[i][j].appear == 2){
-                  newBall(j * BRICKWIDTH + BRICKWIDTH/2, i * BRICKHEIGHT + BRICKHEIGHT/2, radius + 3);
+                if (bricks[i][j].appear == 2) {
+                    newBall(j * BRICKWIDTH + BRICKWIDTH / 2, i * BRICKHEIGHT + BRICKHEIGHT / 2, radius + 3);
                 }
                 if (bricks[i][j].appear == 1) {
-                    rect(j * BRICKWIDTH, i * BRICKHEIGHT
-                        , BRICKWIDTH - PADDING, BRICKHEIGHT - PADDING, i, j);
+                    rect(j * BRICKWIDTH, i * BRICKHEIGHT, BRICKWIDTH, BRICKHEIGHT, i, j);
                 }
             }
         }
 
         //Have We Hit a Bricks?
-        for(i = 0; i < balls.length; ++i){
-            var row = Math.floor((balls[i].y+radius)/(BRICKHEIGHT+PADDING));
-            var col = Math.floor((balls[i].x+radius)/(BRICKWIDTH+PADDING));
-            if(row < NROWS){
-                if(bricks[row][col].appear == 1){
+        for (i = 0; i < balls.length; ++i) {
+            var row = Math.floor((balls[i].y + radius) / (BRICKHEIGHT));
+            var col = Math.floor((balls[i].x + radius) / (BRICKWIDTH));
+            if (row < NROWS) {
+                if (bricks[row][col].appear == 1) {
                     balls[i].dy = -balls[i].dy;
                     balls[i].bounced = 1;
-                    bricks[row][col].number--;// = bricks[row][col].number - 1;
-                    if(bricks[row][col].number == 0){
+                    bricks[row][col].number--; // = bricks[row][col].number - 1;
+                    if (bricks[row][col].number == 0) {
                         bricks[row][col].appear = 0;
                         score++;
                     }
-                }
-                else if(bricks[row][col].appear == 2){
+                } else if (bricks[row][col].appear == 2) {
                     ballCount++;
                     aNewBall = {};
                     aNewBall.dx = 0;
                     aNewBall.dy = 0;
-                    aNewBall.y = HEIGHT - 2*radius;
-                    aNewBall.x = col*BRICKWIDTH + BRICKWIDTH/2;
+                    aNewBall.y = HEIGHT - 2 * radius;
+                    aNewBall.x = col * BRICKWIDTH + BRICKWIDTH / 2;
                     aNewBall.bounced = 0;
                     balls.push(aNewBall);
                     bricks[row][col].appear = 0;
@@ -164,9 +171,18 @@ $(function() {
         // }
     }
 
+    function gameover() {
+        rectgo(0, 0, WIDTH, HEIGHT, "black");
+        text("Game Over", "75px Comic Sans MS", 65, 300, "white");
+        text("SCORE: " + score, "35px Comic Sans MS", 100, 400, "white");
+        rectgo(200, 600, 100, 50, "white");
+        score = 0;
+    }
+
     function clear() {
         ctx.clearRect(0, 0, WIDTH, HEIGHT);
     }
+
     function ball(x, y, r) {
         ctx.fillStyle = '#000000';
         ctx.beginPath();
@@ -174,32 +190,47 @@ $(function() {
         ctx.closePath();
         ctx.fill();
     }
-    function rect(x, y, w, h, i, j){
+
+    function rect(x, y, w, h, i, j) {
         ctx.beginPath();
         ctx.rect(x, y, w, h);
         ctx.closePath();
-        ctx.fillStyle = bricks[i][j].color;//getRndColor();
+        ctx.fillStyle = bricks[i][j].color; //getRndColor();
         ctx.fill();
         ctx.fillStyle = "white";
         ctx.font = "20px Comic Sans MS";
         ctx.fillText(bricks[i][j].number, x + BRICKWIDTH / 2 - 7, y + BRICKHEIGHT / 2 + 5);
     }
-    function newBall(x, y, r){
-      ctx.fillStyle = getRndColor();
-      ctx.beginPath();
-      ctx.arc(x, y, r, 0, Math.PI * 2, true);
-      ctx.closePath();
-      ctx.fill();
+
+    function text(txt, fnt, x, y, c) {
+        ctx.fillStyle = c;
+        ctx.font = fnt;
+        ctx.fillText(txt, x, y);
     }
-    function printBalls(){
-        for(i = 0; i < balls.length; i++){
+
+    function rectgo(x, y, w, h, c) {
+        ctx.fillStyle = w;
+        ctx.fillRect(x, y, w, h);
+    }
+
+    function newBall(x, y, r) {
+        ctx.fillStyle = getRndColor();
+        ctx.beginPath();
+        ctx.arc(x, y, r, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    function printBalls() {
+        for (i = 0; i < balls.length; i++) {
             ball(balls[i].x, balls[i].y, radius);
         }
     }
+
     function init_bricks() {
         NROWS = 5;
         NCOLS = 10;
-        PADDING = 3;
+        //PADDING = 3;
         BRICKWIDTH = (WIDTH / NCOLS);
         BRICKHEIGHT = BRICKWIDTH - BRICKWIDTH / 4;
         //bricks = new Array(NROWS);
@@ -212,13 +243,13 @@ $(function() {
                 brick = {};
                 brick.color = getRndColor();
                 brick.appear = Math.round(Math.random() + 0.1);
-                if(brick.appear == 0){
-                  emptyPositions.push(j);
+                if (brick.appear == 0) {
+                    emptyPositions.push(j);
                 }
-                brick.number = 2;
-                bricks[i][j] = brick;//Math.round(Math.random() + 0.2);
+                brick.number = Math.round(Math.random() * (+3 - +1) + +1);
+                bricks[i][j] = brick; //Math.round(Math.random() + 0.2);
             }
-            randomEmptyPos = Math.floor(Math.random()*(emptyPositions.length));
+            randomEmptyPos = Math.floor(Math.random() * (emptyPositions.length));
             newBallPos = emptyPositions[randomEmptyPos];
             bricks[i][newBallPos].appear = 2;
         }
@@ -230,70 +261,79 @@ $(function() {
     // });
 
 
-    $('#canvas').mousemove(function(e){
+    $('#canvas').mousemove(function (e) {
         // dx = 3;
         // dy = (my - y) * dx / (mx - x);
-          if(e.which == 1 && !gameOn){
+        if (e.which == 1 && !gameOn) {
             clear();
             draw();
             ctx.beginPath();
-            ctx.moveTo(balls[0].x,balls[0].y);
+            ctx.moveTo(balls[0].x, balls[0].y);
             ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
             ctx.closePath();
             ctx.stroke();
-          }
-
+        }
     });
 
-    $('#canvas').mousedown(function(e){
-      ctx.beginPath();
-      ctx.moveTo(balls[0].x,balls[0].y);
-      ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-      ctx.closePath();
-      ctx.stroke();
+    $('#canvas').mousedown(function (e) {
+        if (gameoff == false) {
+            ctx.beginPath();
+            ctx.moveTo(balls[0].x, balls[0].y);
+            ctx.lineTo(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+            ctx.closePath();
+            ctx.stroke();
+        }
     });
 
 
-    $('#canvas').mouseup(function(e){
-      clear();
-
-      if(gameOn == false){
-        gameOn = true;
+    $('#canvas').mouseup(function (e) {
         mx = e.pageX - this.offsetLeft; // mouse x position
         my = e.pageY - this.offsetTop;
-        balls[0].dy = (my - balls[0].y) / 40;
-        balls[0].dx = (mx - balls[0].x) / 40;
-        balls[0].bounced = 0;
-        draw();
-        for(i = 1; i < balls.length; ++i){
-            // while(x++ < 100000000);
-            balls[i].dy = (my - balls[i].y) / 40;
-            balls[i].dx = (mx - balls[i].x) / 40;
-            balls[i].bounced = 0;
-            // draw();
+        if (gameOn == false && gameoff == false) {
+            clear();
+            gameOn = true;
+
+            balls[0].dy = (my - balls[0].y) / 40;
+            balls[0].dx = (mx - balls[0].x) / 40;
+            balls[0].bounced = 0;
+            draw();
+            for (i = 1; i < balls.length; ++i) {
+                // while(x++ < 100000000);
+                balls[i].dy = (my - balls[i].y) / 40;
+                balls[i].dx = (mx - balls[i].x) / 40;
+                balls[i].bounced = 0;
+                // draw();
+            }
+            numBallsOnGround = 0;
+            ballHitGround = 0;
+            firstBallOnGroundIndex = 0;
+            ballsLength = balls.length;
         }
-        numBallsOnGround = 0;
-        ballHitGround = 0;
-        firstBallOnGroundIndex = 0;
-        ballsLength = balls.length;
-      }
+        else if (gameoff == true && mx > 200 && mx < 300 && my > 600 && my < 650) {
+            gameoff = false;
+            balls = [];
+            init();
+            init_bricks();
+        }
     });
 
     function getRndColor() {
-        var r = 255*Math.random()|0,
-        g = 255*Math.random()|0,
-        b = 255*Math.random()|0;
+        var r = 255 * Math.random() | 0,
+            g = 255 * Math.random() | 0,
+            b = 255 * Math.random() | 0;
         return 'rgb(' + r + ',' + g + ',' + b + ')';
     }
 
     function sleep(milliseconds) {
         var start = new Date().getTime();
         for (var i = 0; i < 1e7; i++) {
-            if ((new Date().getTime() - start) > milliseconds){
+            if ((new Date().getTime() - start) > milliseconds) {
                 break;
             }
         }
     }
+
+
 
     init();
 
