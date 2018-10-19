58e3fa19f1b39d0ede237523de3279a7d9151524
Youshin Kim <kim1415@purdue.edu>
2018-10-19 19:36:21 -0400

    .

--- js/user_auth.js
+++ js/user_auth.js
@@ -1,19 +1,14 @@
-  <script src="https://www.gstatic.com/firebasejs/4.10.1/firebase.js"></script>
-  <script>
-    var firebaseEmailAuth; //파이어베이스 email 인증 모듈 전역변수
-    var firebaseDatabase; //파이어베이스 db 모듈 전역변수
-    //파이어 베이스 초기화 코드
-    var config = {
-      apiKey: "AIzaSyA9oNOMkppw2zOBFItjcVvyZ--f1onvaOY",
-      authDomain: "thanksmemo-19012.firebaseapp.com",
-      databaseURL: "https://thanksmemo-19012.firebaseio.com",
-      projectId: "thanksmemo-19012",
-      storageBucket: "thanksmemo-19012.appspot.com",
-      messagingSenderId: "482108959359"
-    };
-    firebase.initializeApp(config);
- 
-    //firebaseEmailAuth = firebase.auth();
-    //firebaseDatabase = firebase.database();
- 
-  </script>
+var firebaseEmailAuth; //파이어베이스 email 인증 모듈 전역변수
+var firebaseDatabase; //파이어베이스 db 모듈 전역변수
+//파이어 베이스 초기화 코드
+var config = {
+  apiKey: "AIzaSyA9oNOMkppw2zOBFItjcVvyZ--f1onvaOY",
+  authDomain: "thanksmemo-19012.firebaseapp.com",
+  databaseURL: "https://thanksmemo-19012.firebaseio.com",
+  projectId: "thanksmemo-19012",
+  storageBucket: "thanksmemo-19012.appspot.com",
+  messagingSenderId: "482108959359"
+};
+firebase.initializeApp(config);
+//firebaseEmailAuth = firebase.auth();
+//firebaseDatabase = firebase.database();
